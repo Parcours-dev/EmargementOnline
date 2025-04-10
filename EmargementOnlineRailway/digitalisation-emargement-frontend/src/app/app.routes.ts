c1ddc97db3ac71fe5872bdfd56cd16bd651e96ad
Parcours <devaletpaul@gmail.com>
2025-04-10 17:36:22 +0200

    fix arbo angular dist v2 mdrr

--- EmargementOnlineRailway/digitalisation-emargement-frontend/src/app/app.routes.ts
+++ EmargementOnlineRailway/digitalisation-emargement-frontend/src/app/app.routes.ts
@@ -1,50 +1,33 @@
 import { Routes } from '@angular/router';
-import { ACCUEIL, DASHBOARD_CFA, DASHBOARD_ETUDIANT, DASHBOARD_PROFESSEUR, RAPPORT_ABSENCE } from './shared/constantes/liens.const';
 
-export const routes: Routes = [
-    {
-        path: ACCUEIL,
-        loadComponent: () =>
-            import("./modules/pages/accueil/accueil.component").then(
-                (mod) => mod.AccueilComponent,
-            ),
-    },
-    {
-        path: DASHBOARD_ETUDIANT,
-        loadComponent: () =>
-            import("./modules/pages/etudiant/dashboard-etudiant/dashboard-etudiant.component").then(
-                (mod) => mod.DashBoardEtudiantComponent,
-            ),
-    },
-    {
-        path: DASHBOARD_PROFESSEUR,
-        loadComponent: () =>
-            import("./modules/pages/professeur/dashboard-professeur/dashboard-professeur.component").then(
-                (mod) => mod.DashBoardProfesseurComponent,
-            ),
-    },
-    {
-        path: DASHBOARD_CFA,
-        loadComponent: () =>
-            import("./modules/pages/cfa/dashboard-cfa/dashboard-cfa.component").then(
-                (mod) => mod.DashBoardCfaComponent,
-            ),
-    },
-    {
-        path: RAPPORT_ABSENCE,
-        loadComponent: () =>
-            import("./modules/pages/cfa/rapport-absence/rapport-absence.component").then(
-                (mod) => mod.RapportAbsenceComponent,
-            ),
-    },
+export const routes = [
+  {
+    path: 'accueil',
+    loadComponent: () => import('./modules/pages/accueil/accueil.component').then(m => m.AccueilComponent)
+  },
+  {
+    path: 'dashboard-etudiant',
+    loadComponent: () => import('./modules/pages/etudiant/dashboard-etudiant/dashboard-etudiant.component').then(m => m.DashBoardEtudiantComponent)
+  },
+  {
+    path: 'dashboard-professeur',
+    loadComponent: () => import('./modules/pages/professeur/dashboard-professeur/dashboard-professeur.component').then(m => m.DashBoardProfesseurComponent)
+  },
+  {
+    path: 'dashboard-cfa',
+    loadComponent: () => import('./modules/pages/cfa/dashboard-cfa/dashboard-cfa.component').then(m => m.DashBoardCfaComponent)
+  },
+  {
+    path: 'rapport-absence',
+    loadComponent: () => import('./modules/pages/cfa/rapport-absence/rapport-absence.component').then(m => m.RapportAbsenceComponent)
+  },
   {
     path: 'generation-qr',
     loadComponent: () => import('./modules/pages/professeur/dashboard-professeur/generation-qr.component').then(m => m.GenerationQrComponent)
   },
-    {
-        path: "**",
-        redirectTo: ACCUEIL,
-        pathMatch: "full"
-    },
-
-];
+  {
+    path: '**',
+    redirectTo: 'accueil',
+    pathMatch: 'full'
+  }
+] as Routes; // 👈 force le typage ici
