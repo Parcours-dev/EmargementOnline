c7525d4c95b4f6327b6c1f9979f84d7f8b723b3a
Parcours <devaletpaul@gmail.com>
2025-04-24 17:01:43 +0200

    Add Modif Matheo v2 - fix app.routes

--- EmargementOnlineRailway/digitalisation-emargement-frontend/src/app/app.routes.ts
+++ EmargementOnlineRailway/digitalisation-emargement-frontend/src/app/app.routes.ts
@@ -1,50 +1,80 @@
 import { Routes } from '@angular/router';
-import { ACCUEIL, DASHBOARD_CFA, DASHBOARD_ETUDIANT, DASHBOARD_PROFESSEUR, RAPPORT_ABSENCE } from './shared/constantes/liens.const';
+import {
+  ACCUEIL,
+  DASHBOARD_CFA,
+  DASHBOARD_ETUDIANT,
+  DASHBOARD_PROFESSEUR,
+  PAGE_ERREUR,
+  RAPPORT_ABSENCE
+} from './shared/constantes/liens.const';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: ACCUEIL,
     loadComponent: () =>
-      import("./modules/pages/accueil/accueil.component").then(
+      import('./modules/pages/accueil/accueil.component').then(
         (mod) => mod.AccueilComponent,
       ),
   },
   {
     path: DASHBOARD_ETUDIANT,
     loadComponent: () =>
-      import("./modules/pages/etudiant/dashboard-etudiant/dashboard-etudiant.component").then(
+      import('./modules/pages/etudiant/dashboard-etudiant/dashboard-etudiant.component').then(
         (mod) => mod.DashBoardEtudiantComponent,
       ),
+    canActivate: [AuthGuard]
   },
   {
     path: DASHBOARD_PROFESSEUR,
     loadComponent: () =>
-      import("./modules/pages/professeur/dashboard-professeur/dashboard-professeur.component").then(
+      import('./modules/pages/professeur/dashboard-professeur/dashboard-professeur.component').then(
         (mod) => mod.DashBoardProfesseurComponent,
       ),
+    canActivate: [AuthGuard]
   },
   {
     path: DASHBOARD_CFA,
     loadComponent: () =>
-      import("./modules/pages/cfa/dashboard-cfa/dashboard-cfa.component").then(
+      import('./modules/pages/cfa/dashboard-cfa/dashboard-cfa.component').then(
         (mod) => mod.DashBoardCfaComponent,
       ),
+    canActivate: [AuthGuard]
   },
   {
     path: RAPPORT_ABSENCE,
     loadComponent: () =>
-      import("./modules/pages/cfa/rapport-absence/rapport-absence.component").then(
+      import('./modules/pages/cfa/rapport-absence/rapport-absence.component').then(
         (mod) => mod.RapportAbsenceComponent,
       ),
+    canActivate: [AuthGuard]
   },
   {
     path: 'generation-qr',
-    loadComponent: () => import('./modules/pages/professeur/dashboard-professeur/generation-qr.component').then(m => m.GenerationQrComponent)
+    loadComponent: () =>
+      import('./modules/pages/professeur/dashboard-professeur/generation-qr.component').then(
+        m => m.GenerationQrComponent
+      ),
+    canActivate: [AuthGuard]
   },
   {
-    path: "**",
-    redirectTo: ACCUEIL,
-    pathMatch: "full"
+    path: 'scan/:token',
+    loadComponent: () =>
+      import('./modules/pages/etudiant/scan-qr/scan-qr.component').then(
+        m => m.ScanQrComponent
+      ),
+    canActivate: [AuthGuard]
   },
-
+  {
+    path: PAGE_ERREUR,
+    loadComponent: () =>
+      import('./modules/pages/page-erreur/page-erreur.component').then(
+        (mod) => mod.PageErreurComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: ACCUEIL,
+    pathMatch: 'full'
+  }
 ];
